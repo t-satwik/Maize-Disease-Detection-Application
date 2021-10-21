9290c02f72bdbd80360f961e09d0099d73283d4b
t-satwik <tallurisatvik800@gmail.com>
2021-10-21 14:56:46 +0530

    Backend Update to handle images and other attributes

--- Backend/maize_detetion_backend/maize_api/models.py
+++ Backend/maize_detetion_backend/maize_api/models.py
@@ -1,14 +1,31 @@
 from django.db import models
+import requests
+
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
 
 # Create your models here.
+import base64
 
 class Data(models.Model):
     # timestamp = models.TextField(input_formats=('%Y-%m-%dT%H:%M:%S+0000',))
-    timestamp = models.TextField()
+    time_stamp = models.TextField(null=True)
     latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
-    user_id=models.IntegerField(null=False, blank=False)
-    # image=models.ImageField(upload_to='uploads/', height_field=224, width_field=224, max_length=100)
+    user_id=models.TextField(null=False, blank=False)
+    encoded_image=models.TextField(null=True)
+    predicted_class=models.TextField(null=True)
+    image_path=models.TextField(null=True)
+    crop_type=models.TextField(null=True)
+
 #    classification = models.CharField(max_length=100)
 #    language = models.CharField(max_length=100)
 
+def save_image_from_url(model_obj, url):
+    r = requests.get(url)
+
+    img_temp = NamedTemporaryFile(delete=True)
+    img_temp.write(r.content)
+    img_temp.flush()
+
+    model_obj.image.save("image.jpg", File(img_temp), save=True)
--- Backend/maize_detetion_backend/maize_api/views.py
+++ Backend/maize_detetion_backend/maize_api/views.py
@@ -7,22 +7,56 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 import sys 
+import os
+
+from django.core.files import File  # you need this somewhere
+import urllib
+
+
 # class DataViewSet(viewsets.ModelViewSet):
 #    queryset = Data.objects.all()
 #    serializer_class = DataSerializer
-
+import base64
 @api_view(['POST'])
 def setData(request):
     try:
         data = request.data
         data_obj = Data()
-        data_obj.timestamp = data['timestamp']
+        data_obj.time_stamp = data['time_stamp']
+        time_stamp_str=str(data['time_stamp'])
         data_obj.latitude = data['latitude']
         data_obj.longitude = data['longitude']
         data_obj.user_id = data['user_id']
-        # data_obj.image = data['image']
+        data_obj.predicted_class = data['predicted_class']
+        data_obj.crop_type = data['crop_type']
+
+        encoded_image_str=str(data['encoded_image'])
+        data_obj.encoded_image = encoded_image_str
+
+        imgdata = base64.b64decode(encoded_image_str) 
+        # filename = '{}_{}.png'.format(data['user_id'], data['time_stamp'])
+        time_stamp_str=time_stamp_str=str(data['time_stamp'])
+        time_stamp_str=time_stamp_str.replace('/', '-')
+        time_stamp_str=time_stamp_str.replace(' ', '_')
+        time_stamp_str=time_stamp_str.replace(':', '-')
+
+        user_id_str=str(data['user_id'])
+        user_id_str=user_id_str.replace('/', '-')
+        user_id_str=user_id_str.replace(' ', '_')
+        time_stamp_str=time_stamp_str.replace(':', '-')
+
+        # filename='D:/Semester V/RnD/Backend/maize_detetion_backend/uploads/'+time_stamp_str+'_'+user_id_str+'.png'
+        imagePath=str('D:/Semester V/RnD/Backend/maize_detetion_backend/uploads/'+time_stamp_str+'_'+user_id_str+'.png')
+        # print('uploads/'+time_stamp_str+'_'+user_id_str+'.png')
+        with open(imagePath, 'wb') as f:
+            f.write(imgdata)
+        data_obj.image_path=imagePath
+        # result = urllib.urlretrieve(filename) # image_url is a URL to an image
+
+
         data_obj.save()
         return Response({"message":"Data Sent"}, status=status.HTTP_200_OK )
     except Exception:
         print(sys.exc_info())
         return Response({"message":"Bad Request - python Exception"}, status=status.HTTP_400_BAD_REQUEST )
+        # return Response({"message":'uploads/'+time_stamp_str+'_'+user_id_str+'.png'}, status=status.HTTP_400_BAD_REQUEST )
